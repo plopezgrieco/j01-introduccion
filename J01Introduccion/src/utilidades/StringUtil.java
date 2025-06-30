b9d52240b514c3fcfa49e30d846b5b11ab0959e3
plopezgrieco <plopezgrieco@gmail.com>
2025-06-30 21:42:15 +0200

    arreglo StringUtil

--- J01Introduccion/src/utilidades/StringUtil.java
+++ J01Introduccion/src/utilidades/StringUtil.java
@@ -18,9 +18,19 @@ public class StringUtil {
 //		System.out.println(indexOf("Hola", "a"));
 //		System.out.println(indexOf("Hola", "Hola"));
 
-		System.out.println(replace("Hola que tal", "que", "x"));
-		System.out.println(replace("Hola que tal", "que", "xxx"));
-		System.out.println(replace("Hola que tal", "que", "xxxxx"));
+//		System.out.println(replace("Hola que tal", "que", "x"));
+//		System.out.println(replace("Hola que tal", "que", "xxx"));
+//		System.out.println(replace("Hola que tal", "que", "xxxxx"));
+
+//		System.out.println(capitalize("HOLA QUE TAL"));
+//		System.out.println(capitalize("HOLA"));
+
+//		System.out.println(cuentaPalabras("Esto es una frase largar de 8 palabras"));
+//		System.out.println(cuentaPalabras("Esto"));
+//		System.out.println(cuentaPalabras(""));
+
+		System.out.println(cuentaParrafos("En un lugar de la Mancha \n cuyo nombre no me acuerdo"));
+		System.out.println(cuentaParrafos("En un lugar de la Mancha"));
 	}
 
 	public static String insert(String cadena, String insertado, int pos) {
@@ -45,25 +55,48 @@ public class StringUtil {
 		return resu;
 	}
 
-	
-	//	NO FUNCIONA
 	public static String replace(String cadena, String buscado, String reemplazo) {
-		if(indexOf(cadena, buscado) == -1)
+		int idxInicio = indexOf(cadena, buscado);
+		if(idxInicio == -1)
 			return cadena;
-		return cadena.substring(0, indexOf(cadena, buscado)) + reemplazo + 
-				(buscado.length() > reemplazo.length() ? cadena.substring(cadena.length()-buscado.length() - 1) 
-													   : cadena.substring(cadena.length()-reemplazo.length() - 1));
+		String inicio = cadena.substring(0, idxInicio);
+		String fin = cadena.substring(idxInicio + buscado.length());
+		return inicio + reemplazo + fin;
 	}
 	
 	public static String capitalize(String cadena) {
-		return null;
+		cadena = cadena.toLowerCase().trim();
+		cadena = Character.toUpperCase(cadena.charAt(0)) + cadena.substring(1);
+		int idx = 0;
+		while((idx = cadena.indexOf(" ", idx + 1)) != -1) {
+			cadena = cadena.substring(0, idx + 1) + Character.toUpperCase(cadena.charAt(idx + 1)) + cadena.substring(idx + 2);
+		}
+		return cadena;
 	}
 	
 	public static int cuentaPalabras(String cadena) {
-		return 0;
+		if (isEmpty(cadena))
+			return 0;
+		cadena = cadena.trim();
+		int idx = 0, cont = 0;
+		while((idx = cadena.indexOf(" ", idx + 1)) != -1) {
+			cont++;
+		}
+		return cont + 1;
 	}
 	
 	public static int cuentaParrafos(String cadena) {
-		return 0;
+		if (isEmpty(cadena))
+			return 0;
+		cadena = cadena.trim();
+		int idx = 0, cont = 0;
+		while((idx = cadena.indexOf("\n", idx + 1)) != -1) {
+			cont++;
+		}
+		return cont + 1;
+	}
+	
+	private static boolean isEmpty(String cadena) {
+		return cadena == null || cadena.trim().length() == 0;
 	}
 }
